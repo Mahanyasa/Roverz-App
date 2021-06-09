a49c7d898b83d8e43c7db17775716993f676f6f7
MahanyasaK-ardue <Mahanyasak@outlook.com>
2021-06-09 14:00:35 +0530

    Rovers App

--- app/src/main/java/com/projectyatri/newsappv32/HomeActivity.kt
+++ app/src/main/java/com/projectyatri/newsappv32/HomeActivity.kt
@@ -77,7 +77,7 @@ class   HomeActivity : AppCompatActivity(), NewsItemClicked {
                 }
                 R.id.web -> {
                     val openURL = Intent(android.content.Intent.ACTION_VIEW)
-                    openURL.data = Uri.parse("https://crz2qdzfz7tk6lci869b9a-on.drv.tw/Wedsite/Ghoradeshwar%20web/Ghoradeshwar.html")
+                    openURL.data = Uri.parse("https://crz2qdzfz7tk6lci869b9a-on.drv.tw/Rovers Database/")
                     startActivity(openURL)
                 }
                 R.id.chat -> {
